f9b1ae3dcb6e38b177fba9f642c410f7edaecc6f
Hayden-Walpole <Haydenwalpole@gmail.com>
2023-08-28 09:22:42 -0600

    .

---
+++ CS-1410 Java/CS1410/src/Lab01/MyFirstClass.java
@@ -0,0 +1,12 @@
+package Lab01;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!");
+		System.out.println(multiply(2,6));
+	}
+	public static double multiply(double number1, double number2) {
+		return number1 * number2;
+	}
+}
