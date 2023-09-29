d840c78ef4644d06e3382f4a04258efd1e401292
haydenwalpole <hwalpole@canyonsleeper.com>
2023-09-29 12:56:01 -0600

    .

--- CS-1410 Java/CS1410/src/assign04/Point2D.java
+++ CS-1410 Java/CS1410/src/assign04/Point2D.java
@@ -1,21 +1,31 @@
 package assign04;
-/**
- * @author Hayden Walpole
- * @version Sept. 25 2023
- */
 public class Point2D {
+	/** this class handles objects of x,y points. 
+	 * @author Hayden Walpole
+	 * @version Sept. 25 2023
+	 */
 	private int xVal;
 	private int yVal;
 	
 	public Point2D() {
+		/**Constructor, creates a Point2D object with the point(0,0)
+		 * 
+		 */
 		this.xVal=0;
 		this.yVal=0;
 	}
 	public Point2D(int x, int y) {
+		/**Creates a Point2D object with the specified point
+		 * @param x- int value for X-value of object
+		 * @param y- int value for Y-value of object
+		 */
 		this.xVal=x;
 		this.yVal=y;
 	}
 	public int getX() {
+		/**Returns the 
+		 * 
+		 */
 		return this.xVal;
 	}
 	public int getY() {
